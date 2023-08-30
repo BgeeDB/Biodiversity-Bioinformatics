8bb3a4d41bc9f97f21966340f2e193e2c46844ca
Alessandro Brandulas Cammarata <alessandro.brandulas.cammarata@gmail.com>
2023-08-30 17:13:24 +0200

    initialized the repo

---
+++ README.md
@@ -0,0 +1,3 @@
+##SIB course 2023 - Biodiversity bioinformatics
+
+This repository contains the materials for the practical exercises of Day 3 of the course:
---
+++ envs/Bgee_packages.R
@@ -0,0 +1,5 @@
+if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install(version = "3.17")
+BiocManager::install("BgeeDB")
+BiocManager::install("BgeeCall")
