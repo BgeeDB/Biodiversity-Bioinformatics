e33565f9da08d6a191dd8cac88c48bfe8a7b85b1
Alessandro Brandulas Cammarata <alessandro.brandulas.cammarata@gmail.com>
2023-08-31 21:42:13 +0200

    moved ggfortify installation

--- envs/Bgee_packages.R
+++ envs/Bgee_packages.R
@@ -1,3 +1,4 @@
+install.packages("ggfortify", repos = "http://cran.us.r-project.org")
 if (!require("BiocManager", quietly = TRUE))
     install.packages("BiocManager",repos = "http://cran.us.r-project.org")
 BiocManager::install(version = "3.16")
