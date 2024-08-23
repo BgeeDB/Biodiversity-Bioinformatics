1c552b6d4e6b3bdf11128498ab4690ad85cbf344
Julien Wollbrett <julien.wollbrett@unil.ch>
2024-08-23 16:14:35 +0200

    update script to adapt to the new format of input data

--- scripts/pca_generation.R
+++ scripts/pca_generation.R
@@ -1,11 +1,20 @@
 install.packages("ggfortify", repos = "http://cran.us.r-project.org")
 library(ggfortify)
 
-expression_dataset = read.table("/workspace/Biodiversity-Bioinformatics/data/pca_dataset")
-pca_res <- prcomp(expression_dataset[4:length(expression_dataset)], scale. = TRUE)
+expression_dataset = read.table(file = gzfile("/workspace/Biodiversity-Bioinformatics/data/pca_dataset.tsv.gz"), header = TRUE, sep = "\t")
+
+#replace type of speciesId from numeric to character
+expression_dataset$speciesId <- as.character(expression_dataset$speciesId)
+
+#run the pca
+pca_res <- prcomp(expression_dataset[5:length(expression_dataset)], scale. = TRUE)
+
+#plot result per species
 jpeg(file="/workspace/Biodiversity-Bioinformatics/figures/Species_PCA.jpeg")
-autoplot(pca_res, data = expression_dataset, colour = 'Label')
+autoplot(pca_res, data = expression_dataset, colour = 'speciesId')
 dev.off()
+
+#plot results per organ
 jpeg(file="/workspace/Biodiversity-Bioinformatics/figures/Organ_PCA.jpeg")
-autoplot(pca_res, data = expression_dataset, colour = 'Organ')
+autoplot(pca_res, data = expression_dataset, colour = 'anatEntityName')
 dev.off()
