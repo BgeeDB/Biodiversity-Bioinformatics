437656a56599cb1bc0e9581882613ffa79de28b0
Alessandro Brandulas Cammarata <alessandro.brandulas.cammarata@gmail.com>
2025-08-13 16:46:27 +0200

    Replaced speciesId with the species names for clarity and added the figures directly

--- scripts/pca_generation.R
+++ scripts/pca_generation.R
@@ -17,37 +17,46 @@ if(file.exists(pca_file_name)) {
 # replace type of speciesId from numeric to character
 expression_dataset$speciesId <- as.character(expression_dataset$speciesId)
 
+
 ####################
 # Run PCAs
 # run the pca for post-embryonic stages
 pca_nonemb <- prcomp(expression_dataset[which(expression_dataset$devStageName=="post-embryonic stage"),5:length(expression_dataset)], scale. = TRUE)
 #run the pca for embryonic stages
 pca_emb <- prcomp(expression_dataset[which(expression_dataset$devStageName=="embryo stage"),5:length(expression_dataset)], scale. = TRUE)
-
 ####################
-# Plot result post-embryonic stages
-# coloring per species
-jpeg(file="/workspace/Biodiversity-Bioinformatics/figures/PCA_post-embryonic_ColoredbySpecies.jpeg")
-a<-autoplot(pca_nonemb, data = expression_dataset[which(expression_dataset$devStageName=="post-embryonic stage"),], colour = 'speciesId')
-a + scale_color_manual(values = c("black","grey50","orange","forestgreen","red","purple")) 
-dev.off()
+# Mapping speciesId to species names
+species_names <- c("9031" = "Chicken",
+                   "9544" = "Macaque", 
+                   "9606" = "Human",
+                   "9986" = "Rabbit",
+                   "10090" = "Mouse",
+                   "10116" = "Rat")
 
+# Create a new column with species names
+expression_dataset$speciesName <- species_names[expression_dataset$speciesId]
+####################
+# Plot using species names for coloring
+jpeg(file="PCA_post-embryonic_ColoredbySpecies.jpeg")
+a <- autoplot(pca_nonemb, 
+              data = expression_dataset[which(expression_dataset$devStageName=="post-embryonic stage"),], 
+              colour = 'speciesName')
+a + scale_color_manual(values = c("black","grey50","orange","forestgreen","red","purple"))
+dev.off()
 # coloring per organ
-jpeg(file="/workspace/Biodiversity-Bioinformatics/figures/PCA_post-embryonic_ColoredbyOrgan.jpeg")
+jpeg(file="PCA_post-embryonic_ColoredbyOrgan.jpeg")
 a<-autoplot(pca_nonemb, data = expression_dataset[which(expression_dataset$devStageName=="post-embryonic stage"),], colour = 'anatEntityName')
-a + scale_color_manual(values = c("black","grey50","orange","forestgreen","red","purple","blue")) 
+a + scale_color_manual(values = c("black","grey50","orange","forestgreen","red","purple","blue"))
 dev.off()
-
 ####################
 # Plot result embryonic stages
 # coloring per species
-jpeg(file="/workspace/Biodiversity-Bioinformatics/figures/PCA_embryonic_ColoredbySpecies.jpeg")
-a<-autoplot(pca_emb, data = expression_dataset[which(expression_dataset$devStageName=="embryo stage"),], colour = 'speciesId')
-a + scale_color_manual(values = c("black","grey50","orange","forestgreen","red","purple")) 
+jpeg(file="PCA_embryonic_ColoredbySpecies.jpeg")
+a<-autoplot(pca_emb, data = expression_dataset[which(expression_dataset$devStageName=="embryo stage"),], colour = 'speciesName')
+a + scale_color_manual(values = c("black","grey50","orange","forestgreen","red","purple"))
 dev.off()
-
 # coloring per organ
-jpeg(file="/workspace/Biodiversity-Bioinformatics/figures/PCA_embryonic_ColoredbyOrgan.jpeg")
+jpeg(file="PCA_embryonic_ColoredbyOrgan.jpeg")
 a<-autoplot(pca_emb, data = expression_dataset[which(expression_dataset$devStageName=="embryo stage"),], colour = 'anatEntityName')
-a + scale_color_manual(values = c("black","grey50","orange","forestgreen","red","purple","blue")) 
+a + scale_color_manual(values = c("black","grey50","orange","forestgreen","red","purple","blue"))
 dev.off()
